4d2e9ea83ce1956f8ad74f38614e4948e9ff59c6
mumis <emil.wertwein@gmail.com>
2020-01-08 14:19:43 +0100

    add greeting and typing message

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,31 @@ export default {
 
 <style>
   * {
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Ubuntu', sans-serif;
   }
   body {
     margin: 0;
   }
-  h1, h2, h3, p, small {
+  h1,h2,h3,h4 {
+    color: rgb(74, 42, 24);
+    font-weight: normal;
     margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: lighter;
+  }
+  h1 {
+    font-size: 45px;
+  }
+  h2 {
+    font-size: 35px;
+  }
+  h3 {
+    font-size: 30px;
+  }
+  h4 {
+    font-size: 25px;
+  }
+  p,small {
+    color: rgb(178, 112, 78);
   }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
 	<section>
-		<h2> {{this.message}}</h2>
+		<h2> {{this.greeting}}</h2>
+		<h4> {{this.introMessageTyped}} <span>|</span> </h4>
+		<small> Front end development / UI and UX-Designer </small>	
+		<div class="mouse">
+			<span> </span>
+		</div>
 	</section>
 </template>
 
@@ -9,7 +14,7 @@ export default {
 	name: 'Home',
 	data: function() {
 		return {
-			messageRandom: [
+			greetingList: [
 				{ id: 1, text: "Hello," },
 				{ id: 2, text: "Hello there," },
 				{ id: 3, text: "Howdy!" },
@@ -18,13 +23,14 @@ export default {
 				{ id: 6, text: "Aloha!" },
 				{ id: 7, text: "Greetings!" },
 			],
-			messagePerHour: {
+			greetingHourly: {
 				morning: "Good Morning,",
 				afternoon: "Good Afternoon,",
 				evening: "Good Evening," 
-			}
-			,
-			message: "",
+			},
+			greeting: "",
+			introMessage: "I'm Emil, a web developer.",
+			introMessageTyped: "",
 		}
 	},
 	methods: {
@@ -33,41 +39,71 @@ export default {
 			const hour = now.getHours();
 			if (hour < 12) {
 				if (Math.random() * 3 < 2) {
-					this.message = this.messagePerHour.morning;
+					this.greeting = this.greetingHourly.morning;
 				}
 				else {
-					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+					this.greeting = this.greetingList[Math.round(Math.random() * this.greetingList.length - 1)].text;
 				}
 			}
 			else if (hour >= 12 && hour <= 17) {
 				if (Math.random() * 3 < 2) {
-					this.message = this.messagePerHour.afternoon;
+					this.greeting = this.greetingHourly.afternoon;
 				}
 				else {
-					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+					this.greeting = this.greetingList[Math.round(Math.random() * this.greetingList.length - 1)].text;
 				}
 			} else {
 				if (Math.random() * 3 < 2) {
-					this.message = this.messagePerHour.evening;
+					this.greeting = this.greetingHourly.evening;
 				}
 				else {
-					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+					this.greeting = this.greetingList[Math.round(Math.random() * this.greetingList.length - 1)].text;
 				}
 			}
 		},
+		startTypeIntro: function() {
+			for (let i in this.introMessage) {
+					setTimeout(() => {
+							this.introMessageTyped = this.introMessageTyped + this.introMessage[i];
+					}, 130*i);
+			}
+		}
 	},
 	created() {
 		this.chooseMessage();
+		this.startTypeIntro();
 	},
 }
 </script>
 
-<style>
+<style scoped>
+	@keyframes blink {
+    0% { opacity: 1; }
+    49% { opacity: 1; }
+    50% { opacity: 0; }
+    100% { opacity: 0; }
+	}
+
+	span {
+		animation: blink;
+		animation-duration: 0.9s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-delay: 2.7s;
+	}
+
 	section {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+		text-align: center;
 		height: 100vh;
+		padding: 0 4px;
 		background-color: rgb(255, 218, 208);
+	}
+
+	h2 {
+		margin-bottom: 15px;
 	}
 </style>
