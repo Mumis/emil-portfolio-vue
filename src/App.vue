4780d0eca74f18321f672084f302822111ba1d8c
mumis <emil.wertwein@gmail.com>
2020-01-08 13:15:18 +0100

    add boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './pages/Index.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: 'Montserrat', sans-serif;
+  }
+  body {
+    margin: 0;
+  }
+  h1, h2, h3, p, small {
+    margin: 0;
+  }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+	<section>
+		<h2> {{this.message}}</h2>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	data: function() {
+		return {
+			messageRandom: [
+				{ id: 1, text: "Hello," },
+				{ id: 2, text: "Hello there," },
+				{ id: 3, text: "Howdy!" },
+				{ id: 4, text: "Good to see you," },
+				{ id: 5, text: "Nice to meet you!" },
+				{ id: 6, text: "Aloha!" },
+				{ id: 7, text: "Greetings!" },
+			],
+			messagePerHour: {
+				morning: "Good Morning,",
+				afternoon: "Good Afternoon,",
+				evening: "Good Evening," 
+			}
+			,
+			message: "",
+		}
+	},
+	methods: {
+		chooseMessage: function() {
+			const now = new Date();
+			const hour = now.getHours();
+			if (hour < 12) {
+				if (Math.random() * 3 < 2) {
+					this.message = this.messagePerHour.morning;
+				}
+				else {
+					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+				}
+			}
+			else if (hour >= 12 && hour <= 17) {
+				if (Math.random() * 3 < 2) {
+					this.message = this.messagePerHour.afternoon;
+				}
+				else {
+					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+				}
+			} else {
+				if (Math.random() * 3 < 2) {
+					this.message = this.messagePerHour.evening;
+				}
+				else {
+					this.message = this.messageRandom[Math.round(Math.random() * this.messageRandom.length - 1)].text;
+				}
+			}
+		},
+	},
+	created() {
+		this.chooseMessage();
+	},
+}
+</script>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		background-color: rgb(255, 218, 208);
+	}
+</style>
