c92a3c6346c393dad2deba6d20a61cb72456d4a0
mumis <emil.wertwein@gmail.com>
2020-03-12 10:04:04 +0100

    add icon component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,28 +1,18 @@
 <template>
- <section id="about">
-   <h2> About me </h2>
-   <p>Hello! I'm a front-end developer and I'm currently looking for an internship. I wish to create functional and inspirational websites for my future clients.</p>
+  <section id="about">
+    <h2> About me </h2>
+    <p>Hello! I'm a front-end developer and I'm currently looking for an internship. I wish to create functional and inspirational websites for my future clients.</p>
      
     <p>Check out some of my <a href="#projects">projects</a> and don't hesitate to <a href="#contact">contact</a> if you would like to know more!</p>
-   <p>Some of the <span class="highlighted">technologies</span> and <span class="highlighted">tools</span> I have worked with:</p>
-   <ul>
-    <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.react" alt="React icon" srcset=""><p>React.js</p></li></a>
-    <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.gatsby" alt="Gatsby icon" srcset=""><p>Gatsby.js</p></li></a>
-    <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.vue" alt="Vue icon" srcset=""><p>Vue.js</p></li></a>
-    <a href="http://firebase.google.com" target="_blank" rel="noopener noreferrer"><li><img :src="icons.firebase" alt="Firebase icon" srcset=""><p>Firebase</p></li></a>
-    <a href="http://https://www.netlify.com/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.netlify" alt="Netlify icon" srcset=""><p>Netlify</p></li></a>
-    <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.git" alt="Git icon" srcset=""><p>Git</p></li></a>
-    <a href="http://https://github.com/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.github" alt="Github icon" srcset=""><p>Github</p></li></a>
-    <a href="https://threejs.org/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.three" alt="Three icon" srcset=""><p>Three.js</p></li></a>
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer"><li><img :src="icons.vs" alt="VScode icon" srcset=""><p>Visual studio</p></li></a>
-    <a href="https://www.adobe.com" target="_blank" rel="noopener noreferrer"><li><img :src="icons.photoshop" alt="VScode icon" srcset=""><p>Photoshop</p></li></a>
-    <a href="https://www.adobe.com" target="_blank" rel="noopener noreferrer"><li><img :src="icons.xd" alt="VScode icon" srcset=""><p>Adobe XD</p></li></a>
-    <a href="https://krita.org" target="_blank" rel="noopener noreferrer"><li><img :src="icons.krita" alt="Krita icon" srcset=""><p>Krita</p></li></a>
-   </ul>
- </section>
+    <p>Some of the <span class="highlighted">technologies</span> and <span class="highlighted">tools</span> I have worked with:</p>
+    <ul>
+      <li v-for="icon in icons" v-bind:key=icon.key><Icon :icon="icon"/></li>
+    </ul>
+  </section>
 </template>
 
 <script>
+import Icon from './AboutIcon';
 import reactIcon from '../assets/icons/react.svg';
 import gatsbyIcon from '../assets/icons/gatsby.svg';
 import vueIcon from '../assets/icons/vue.svg';
@@ -30,17 +20,20 @@ import firebaseIcon from '../assets/icons/firebase.svg';
 import netlifyIcon from '../assets/icons/netlify.svg';
 import gitIcon from '../assets/icons/git.svg';
 import githubIcon from '../assets/icons/github.svg';
-import threeIcon from '../assets/icons/three.svg'
-import vsIcon from '../assets/icons/vs.svg'
-import photoshopIcon from '../assets/icons/photoshop.svg'
-import xdIcon from '../assets/icons/xd.svg'
-import kritaIcon from '../assets/icons/krita.svg'
+import threeIcon from '../assets/icons/three.svg';
+import vsIcon from '../assets/icons/vs.svg';
+import photoshopIcon from '../assets/icons/photoshop.svg';
+import xdIcon from '../assets/icons/xd.svg';
+import kritaIcon from '../assets/icons/krita.svg';
 
 export default {
   name: 'About',
+  components: {
+    Icon
+  },
   data: function() {
     return {
-      icons: {
+      iconsImages: {
         react: reactIcon,
         gatsby: gatsbyIcon,
         vue: vueIcon,
@@ -53,7 +46,23 @@ export default {
         photoshop: photoshopIcon,
         xd: xdIcon,
         krita: kritaIcon,
-      }
+      },
+      icons: [
+        { name: "React.js", url: "https://www.reactjs.org/", img: reactIcon },
+        { name: "Gatsby.js", url: "https://www.gatsbyjs.org/", img: gatsbyIcon },
+        { name: "Vue.js", url: "https://www.vuejs.org/", img: vueIcon },
+        { name: "Firebase", url: "https://www.firebase.google.com/", img: firebaseIcon },
+        { name: "Netlify", url: "http://https://www.netlify.com/", img: netlifyIcon },
+        { name: "Git", url: "https://git-scm.com/", img: gitIcon },
+        { name: "Github", url: "http://https://github.com/", img: githubIcon },
+        { name: "Three.js", url: "https://threejs.org/", img: threeIcon },
+        { name: "Visual Studio", url: "https://code.visualstudio.com/", img: vsIcon },
+        { name: "Photoshop", url: "https://www.adobe.com/", img: photoshopIcon },
+        { name: "Adobe XD", url: "https://www.adobe.com/", img: xdIcon },
+        { name: "Krita", url: "https://krita.org/", img: kritaIcon }
+      ]
+        
+      
     }
   }
 }
@@ -73,31 +82,25 @@ p {
 }
 a {
   text-decoration: none;
-  flex: 1 0 21%;
 }
 ul {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: space-evenly;
   text-align: left;
 	width: 100%;
   list-style: none;
   padding: 0;
 }
 li {
-    text-align: center;
-    margin: 20px 20px;
-    transition: .3;
-}
-li:hover img {
-  opacity: .5;
-}
+    flex-basis: 25%;
+  }
 img {
   height: 56px;
   transition: opacity 0.3s;
 }
 @media only screen and (max-width: 767px) {
-  a {
+  li {
     flex-basis: 50%;
   }
 }
---
+++ src/components/AboutIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <a v-bind:href="icon.url" target="_blank" rel="noopener noreferrer"><img v-bind:src="icon.img" alt="Icon" srcset=""><p>{{icon.name}}</p></a>
+</template>
+
+<script>
+export default {
+  name: 'AboutIcon',
+  props: {
+    icon: {
+      required: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+a {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	text-align: center;
+	transition: .3;
+	margin: 5px;
+}
+a:hover img {
+  opacity: .5;
+}
+img {
+  height: 56px;
+  transition: opacity 0.3s;
+}
+
+@media only screen and (max-width: 767px) {
+
+}
+</style>
